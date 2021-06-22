ed3b6964aad9d7fb20849aea22a7646e32e9f45a
ky-devblog <53827090+ky-devblog@users.noreply.github.com>
2021-06-22 11:31:44 +0900

    succeeded

--- uno_rx/uno_rx.ino
+++ uno_rx/uno_rx.ino
@@ -2,19 +2,20 @@ int LED = 13;
 
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);
   pinMode(LED, OUTPUT);
 }
 char data;
 void loop() {
-  while(Serial.available()){
-    data = (char)Serial.read();
+  while(Serial1.available()){
+    data = (char)Serial1.read();
     if(data == '1'){
       digitalWrite(LED, HIGH);
-      //delay(1000);
+      delay(1000);
     }
     else if(data == '0'){
       digitalWrite(LED, LOW);
-      //delay(1000);
+      delay(1000);
     }
   }
 }
--- uno_tx/uno_tx.ino
+++ uno_tx/uno_tx.ino
@@ -1,13 +1,14 @@
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  Serial.print('0');
+  Serial1.print('0');
   delay(1000);
-  Serial.print('1');
+  Serial1.print('1');
   delay(1000);
   // put your main code here, to run repeatedly:
 
